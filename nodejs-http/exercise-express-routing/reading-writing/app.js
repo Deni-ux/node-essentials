511df5d7851ea84451b786dcb7ca22e9dfd87d2f
Denisa Haka <denisa.haka@ymail.com>
2022-08-01 22:52:47 +0100

    CRUD implement

--- nodejs-http/exercise-express-routing/reading-writing/app.js
+++ nodejs-http/exercise-express-routing/reading-writing/app.js
@@ -1,27 +1,47 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-let bodyParser = require('body-parser');
+let bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 let products = [];
 
-app.post('/products', function (req, res) {
-  // implement
+//To implement CRUD
+//app.route('/products').get().post().put().delete()
+
+//IMPLEMENT WRITING
+app.post("/products", function (req, res) {
+  //  1st reload: the api responded w an empty array
+  //write data
+  //The new code reads incoming data from req.body and constructs a JavaScript object from it.Next, it's added to the products array. Finally, the new product is returned to the user.
+  const newProduct = { ...req.body, id: products.length + 1 };
+  products = [...products, newProduct];
+  res.json(newProduct);
 });
 
-app.put('/products', function (req, res) {
-  // implement
+//IMPLEMENT ABILITY TO UPDATE DATA
+app.put("/products", function (req, res) {
+  let updatedProduct;
+  products = products.map((p) => {
+    if (p.id === req.body.id) {
+      updatedProduct = { ...p, ...req.body };
+      return updatedProduct;
+    }
+    return p;
+  });
+  res.json(updatedProduct);
 });
 
-app.delete('/products/:id', function (req, res) {
-  // implement
+//IMPLEMENT DELETE
+app.delete("/products/:id", function (req, res) {
+  const deletedProduct = products.find((p) => p.id === +req.params.id);
+  products = products.filter((p) => p.id !== +req.params.id);
+  res.json(deletedProduct);
 });
 
-app.get('/products', (req, res) => {
-  // implement
-})
+app.get("/products", (req, res) => {
+  res.json(products);
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-  
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
